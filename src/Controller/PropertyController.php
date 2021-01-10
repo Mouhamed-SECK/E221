03718ecf9a02739d0d2deb60637c4272f77f743e
medsoseck <medsoseck@gmail.com>
2021-01-10 18:08:36 +0100

    feat: add index method and show method in propertyController

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,20 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PropertyRepository;
 
 class PropertyController extends AbstractController
 {
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/biens", name="property.index")
      */
     public function index(): Response
     {
+        $property = $this->repository->findAllVisible();
+        $property[0]->setIsLoan(false);
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+        dump($property);
+
         return $this->render('property/index.html.twig', [
             'controller_name' => 'PropertyController',
             'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show(Property $property, string $slug): Response
+    {
+        if ($property->getSlug() == $slug) {
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'current_menu' => 'dd',
+            'property' => $property
         ]);
     }
 }
---
+++ src/Entity/Property.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
+
+/**
+ * @ORM\Entity(repositoryClass=PropertyRepository::class)
+ */
+class Property
+{
+
+    const USAGE_TYPE = [
+        0 => 'Bureau',
+        2 => 'Logement'
+    ];
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $badrooms;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $rooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $surface;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $postalCode;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $usageType;
+
+    /**
+     * @ORM\Column(type="dateinterval", nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private $isLoan;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getBadrooms(): ?int
+    {
+        return $this->badrooms;
+    }
+
+    public function setBadrooms(?int $badrooms): self
+    {
+        $this->badrooms = $badrooms;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(?int $rooms): self
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    public function getSurface(): ?int
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(int $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getUsageType(): ?int
+    {
+        return $this->usageType;
+    }
+
+    public function setUsageType(int $usageType): self
+    {
+        $this->usageType = $usageType;
+
+        return $this;
+    }
+
+    public function getDuration(): ?\DateInterval
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?\DateInterval $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getIsLoan(): ?bool
+    {
+        return $this->isLoan;
+    }
+
+    public function setIsLoan(bool $isLoan): self
+    {
+        $this->isLoan = $isLoan;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getFormattedPrice(): string
+    {
+        return number_format($this->price, 0, '', ' ');
+    }
+
+    public function getSlug(): string
+    {
+        return (new Slugify())->slugify($this->title); // hello-world
+    }
+}
