8351d13e84b08cb0aa8d8b46b6d021b7aee0e4a2
medsoseck <medsoseck@gmail.com>
2021-01-14 11:20:52 +0100

    feat: add user registration

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
@@ -33,7 +38,24 @@ class AccountController extends AbstractController
     /**
      * @Route("/register", name="account.register")
      */
-    public function register()
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
     {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getHash());
+
+            $user->setHash($hash);
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return  $this->render('account/signup.html.twig', [
+            'form' => $form->createView()
+
+        ]);
     }
 }
