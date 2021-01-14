b3ac2e6093bf2609c5149eb99d207d8c4ab56cc9
medsoseck <medsoseck@gmail.com>
2021-01-14 06:50:39 +0100

    feat: add authentication in security.yaml and add AccountController

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/login", name="account.login")
+     */
+    public function login(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="account.logout")
+     */
+    public function logout()
+    {
+    }
+
+    /**
+     * @Route("/register", name="account.register")
+     */
+    public function register()
+    {
+    }
+}
